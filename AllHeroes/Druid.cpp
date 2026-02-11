d34e8981d9f1add742a1e0308cb321348f32ba4b
Mrkdevv <aebolm@gmail.com>
2026-02-11 22:18:47 +0200

    Added each hero his abillity

--- AllHeroes/Druid.cpp
+++ AllHeroes/Druid.cpp
@@ -14,3 +14,52 @@ void  Druid::ShowAbilities()
 {
     std::cout << "1) " << Strangleroot_Druid.name << "\n2) " <<Wild_Shape_Druid.name << std::endl;
 }
+void Druid::FirstAbility(Hero *target) {
+    if (this->mana >= GameSettings::Druid::Strangleroot::MANA_COST) {
+        this->mana -= GameSettings::Druid::Strangleroot::MANA_COST;
+        target->health -= GameSettings::Druid::Strangleroot::DAMAGE;
+
+        if (target->health < 0) target->health = 0;
+
+        std::cout << "Successful!" << " target hp = " << target->health << "target mana ="<< target->mana << std::endl;
+        std::cout << "Your hp = " << this->health << " your mana = " << this->mana << std::endl;
+
+        if (rand() % 100 < GameSettings::Druid::Strangleroot::CHANCE_TO_DOUBLE_DAMAGE) {
+            std::cout << "Lucky! You got double damage(second hit!)" << std::endl;
+            target->health -= GameSettings::Druid::Strangleroot::DAMAGE;
+            if (target->health < 0) target->health = 0;
+            std::cout << "Target HP after second arrow: " << target->health << std::endl;
+        }
+    }
+    else {
+        std::cout << "Not enough mana to use Strangeleroot!" << std::endl;
+    }
+}
+
+void Druid::SecondAbility(Hero *target) {
+    if (this->mana >= GameSettings::Druid::WildShape::MANA_COST) {
+        this->mana -= GameSettings::Druid::WildShape::MANA_COST;
+
+        int total_damage = this->damage + GameSettings::Druid::WildShape::ADDED_DAMAGE;
+        target->health -= total_damage;
+        if (target->health < 0) target->health = 0;
+
+        this->health += GameSettings::Druid::WildShape::ADDED_HP;
+        if (this->health > GameSettings::Druid::HP) {
+            this->health = GameSettings::Druid::HP;
+        }
+        std::cout << "Successful! Target hp = " << target->health << " target mana = " << target->mana << std::endl;
+        std::cout << "Your hp = " << this->health << " your mana = " << this->mana << std::endl;
+
+        if (rand() % 100 < GameSettings::Druid::WildShape::CHANCE_TO_VAMPIRISM) {
+            std::cout << "Lucky! You got vampirism: +5 HP" << std::endl;
+            this->health += 5;
+            if (this->health > GameSettings::Druid::HP) {
+                this->health = GameSettings::Druid::HP;
+            }
+        }
+    }
+    else {
+        std::cout << "Not enough mana to use WildShape!" << std::endl;
+    }
+}
--- AllHeroes/SwordMan.cpp
+++ AllHeroes/SwordMan.cpp
@@ -15,3 +15,48 @@ void SwordMan::ShowAbilities()
 {
     std::cout << "1) " << Slashing_Lunge.name << "\n2) " << Sword_Lunge.name << std::endl;
 }
+C++
+void SwordMan::FirstAbility(Hero* target) {
+    if (this->mana >= GameSettings::SwordMan::Slashing_Lunge::MANA_COST) {
+        this->mana -= GameSettings::SwordMan::Slashing_Lunge::MANA_COST;
+        target->health -= GameSettings::SwordMan::Slashing_Lunge::DAMAGE;
+
+        if (target->health < 0) target->health = 0;
+
+        std::cout << "Successful! Target hp = " << target->health << " target mana = " << target->mana << std::endl;
+        std::cout << "Your hp = " << this->health << " your mana = " << this->mana << std::endl;
+
+        if (rand() % 100 < GameSettings::SwordMan::Slashing_Lunge::CHANCE_TO_VAMPIRISM) {
+            std::cout << "Lucky! You got vampirism: +5 HP" << std::endl;
+            this->health += 5;
+            if (this->health > GameSettings::SwordMan::HP) {
+                this->health = GameSettings::SwordMan::HP;
+            }
+        }
+    }
+    else {
+        std::cout << "Not enough mana to use Slashing_Lunge!" << std::endl;
+    }
+}
+
+void SwordMan::SecondAbility(Hero* target) {
+    if (this->mana >= GameSettings::SwordMan::SwordLunge::MANA_COST) {
+        this->mana -= GameSettings::SwordMan::SwordLunge::MANA_COST;
+        target->health -= GameSettings::SwordMan::SwordLunge::DAMAGE;
+
+        if (target->health < 0) target->health = 0;
+
+        std::cout << "Successful! Target hp = " << target->health << " target mana = " << target->mana << std::endl;
+        std::cout << "Your hp = " << this->health << " your mana = " << this->mana << std::endl;
+
+        if (rand() % 100 < GameSettings::SwordMan::SwordLunge::CHANCE_TO_DOUBLE_DAMAGE) {
+            std::cout << "Lucky! You got double damage(second hit!)" << std::endl;
+            target->health -= GameSettings::SwordMan::SwordLunge::DAMAGE;
+            if (target->health < 0) target->health = 0;
+            std::cout << "Target HP after second hit: " << target->health << std::endl;
+        }
+    }
+    else {
+        std::cout << "Not enough mana to use SwordLunge!" << std::endl;
+    }
+}
